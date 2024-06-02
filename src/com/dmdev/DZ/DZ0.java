5e3af99309cda23300bfadc070b844d5f1855478
Danil P <daniil.6499@yandex.ru>
2024-06-02 22:14:55 +0300

    DZ0

--- src/com/dmdev/DZ/DZ0.java
+++ src/com/dmdev/DZ/DZ0.java
@@ -3,7 +3,7 @@ package com.dmdev.DZ;
 public class DZ0 {
 
     public static void main(String[] args) {
-        int a, b, c;
+        int a, b, c, d;
         System.out.println("1   2   3   4");
     }
 }
